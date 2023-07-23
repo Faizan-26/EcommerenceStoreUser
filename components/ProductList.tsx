13d4edc95df9142a1c7263630e5b1f9decfda836
Muhammad Faizan Tariq <115633021+Faizan-26@users.noreply.github.com>
2023-07-23 17:32:39 +0500

    Update ProductList.tsx

--- components/ProductList.tsx
+++ components/ProductList.tsx
@@ -17,12 +17,8 @@ const ProductList: React.FC<ProductListProps> = ({
       <h3 className="font-bold  text-3xl mb-2">{title}</h3>
       {items.length === 0 && <Noresult />}
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 ">
-        {items.map((item) => (
-          <div>
-            {parseInt(item.quantity)>=1 &&   // this will display item only when item quantity greater than 0
+        {items.map((item) => (  
             <ProductCard  key={item.id} data={item} />
-            }
-          </div>
         ))}
       </div>
     </div>
